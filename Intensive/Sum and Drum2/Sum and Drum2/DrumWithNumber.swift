06c53c4daae81ce02b9939e1cf2cc12437ae84de
Matthew Phraxayavong <mphraxa@gmail.com>
2018-11-12 10:26:29 -0800

    [add] code from step 5 of sweet git tutorial

---
+++ Intensive/Sum and Drum2/Sum and Drum2/DrumWithNumber.swift
@@ -0,0 +1,9 @@
+//
+//  DrumWithNumber.swift
+//  Sum and Drum2
+//
+//  Created by Matthew Phraxayavong on 10/19/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
---
+++ Intensive/Sum n Drum/Sum n Drum/Gamescene.swift
@@ -0,0 +1,12 @@
+//
+//  Gamescene.swift
+//  Sum n Drum
+//
+//  Created by Matthew Phraxayavong on 10/17/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+
